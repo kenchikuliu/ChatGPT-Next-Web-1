e1684b4f93eb4177228e15caff278e80cc121d78
kenchikuliu <44759890+kenchikuliu@users.noreply.github.com>
2023-07-18 15:04:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatMarket",
+  description: "Your personal ChatGPT for Marketing.",
   viewport: {
     width: "device-width",
     initialScale: 1,
